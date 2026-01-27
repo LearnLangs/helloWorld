3d9222c9150544bcc2e4eca7d84b4cec0ac608d7
Davisone <67480359+krush-ghod@users.noreply.github.com>
2026-01-26 22:01:24 -0500

    Updated the c file

--- c/helloWorld.c
+++ c/helloWorld.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-
+// note that you always have to remember the ";" - semicolon
 int main() {
-    printf("Hello world, this is my first app in C")
-    return 0
+    printf("Hello world, this is my first app in C");
+    return 0;
 }   
